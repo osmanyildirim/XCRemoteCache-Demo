5d2c3d362e6103e8c0f1e0fcbcef3e2f555ccece
Osman Yıldırım <yildirim.osman@icloud.com>
2022-01-26 02:58:31 +0300

    initial commit

---
+++ XCRemoteCache-Demo_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCRemoteCache-Demo_1
+//
+//  Created by Osman YILDIRIM on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
